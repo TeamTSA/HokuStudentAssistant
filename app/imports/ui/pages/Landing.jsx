3bd33420424bff7f2573154f5277998c662a3775
WendyChen9 <31325518+WendyChen9@users.noreply.github.com>
2018-11-08 14:55:30 -1000

    landing

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -20,8 +20,8 @@ class Landing extends React.Component {
           <Grid>
             <Grid.Row centered>
               <Grid.Column textAlign='center'>
-                <Header as='h1' inverted className='title'> Welcome to Washer Watcher!</Header>
-                <Header as='h3' inverted className='subheader'>Know When a Washer is Available in Your Dorm</Header>
+                <Header as='h1' inverted className='top'>H O K U</Header>
+                <Header as='h3' inverted className='info'>Easily navigate through college using Hoku, an interactive campus map showing campus events, classes, eateries, and bathrooms.</Header>
               </Grid.Column>
             </Grid.Row>
 
@@ -29,6 +29,26 @@ class Landing extends React.Component {
             </Grid.Row>
           </Grid>
 
+          <Grid className='table'>
+            <Grid.Column className='card' width={5}>
+              <Header as='h2'>As a Student...</Header>
+              <Image src='https://mbtskoudsalg.com/images/student-cartoon-png-5.png' />
+            </Grid.Column>
+            <Grid.Column width={11}>
+              <Header as='h5'> With Hoku, viewing your semesters classes is easier than ever. Simply add in your https://mbtskoudsalg.com/images/student-cartoon-png-5.pngcourse CRN to be able to view your classes on the Hoku interactive campus map.</Header>
+            </Grid.Column>
+          </Grid>
+          <Grid>
+            <Grid.Column width={11}>
+              <Header as='h5'>Advertise campus events to the Hoku users. With Hoku, admins are able to add campus events, which will be shown on the Hoku interactive campus maps.</Header>
+            </Grid.Column>
+            <Grid.Column className='card' width={5}>
+              <Header as='h2'>As a Admin...</Header>
+              <Image src='https://s3-ap-southeast-2.amazonaws.com/autom-io-home-assets/Uploads/3-office-workers-graphic.png' />
+            </Grid.Column>
+          </Grid>
+
+          
           <Grid stackable centered columns={3}>
             <Grid.Column textAlign='center'>
               <Icon size='huge' name='group' inverted/>
