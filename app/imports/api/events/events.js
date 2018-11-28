161719fb8fd140f893a87fe2b363d1e3fdbe7148
jkasuya <35503264+jkasuya@users.noreply.github.com>
2018-11-27 14:25:33 -1000

    Changed Event Schema

--- app/imports/api/events/events.js
+++ app/imports/api/events/events.js
@@ -1,15 +1,21 @@
 import { Mongo } from 'meteor/mongo';
 import SimpleSchema from 'simpl-schema';
 
-const eventSchema = new SimpleSchema({
+/** Create a Meteor collection. */
+const Events = new Mongo.Collection('Events');
+
+/** Create a schema to constrain the structure of documents associated with this collection. */
+const EventsSchema = new SimpleSchema({
   event_id: Number,
   eventName: String,
   eventDescription: String,
   eventStartDate: Date,
   eventEndDate: Date,
-  eventType: String, // ex. Athletic, fairs, etc.
-});
+  eventType: String,
+}, { tracker: Tracker });
 
-export const Events = new Mongo.Collection('events');
-
+/** Attach this schema to the collection. */
 Events.attachSchema(eventSchema);
+
+/** Make the collection and schema available to other code. */
+export { Events, EventsSchema };
