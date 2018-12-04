84fdc67115d7d564d21b6f8d0796fdb7cd171c7f
Camelia Lai <camelial@hawaii.edu>
2018-12-03 14:04:48 -1000

    attempting to set up initial entities from settings.development.json

---
+++ app/imports/startup/server/initial-entities.js
@@ -0,0 +1,29 @@
+import { Meteor } from 'meteor/meteor';
+import { Courses } from '../../api/courses/courses';
+import { Locations } from '../../api/locations/locations';
+import { Bathrooms } from '../../api/bathrooms/bathrooms';
+import { FoodPlace } from '../../api/food/foodPlaces';
+
+
+/**
+ * Generic function used to load definitions from the settings.*.json file.
+ * @param name The field in the settings file containing the entity definitions.
+ * @param collection The collection whose define method will be called on each definition.
+ */
+function initEntity(name, collection) {
+  const definitions = Meteor.settings.initialEntities[name] || [];
+  console.log(`Initializing ${definitions.length} ${name}`);
+  definitions.map(definition => collection.define(definition));
+}
+
+/**
+ * Define entities at system startup.  Locations must be defined first
+ */
+Meteor.startup(() => {
+  if (Meteor.settings.initialEntities && Meteor.settings.initialEntities.enabled) {
+    //initEntity('Locations', Locations);
+    initEntity('Courses', Courses);
+    //initEntity('Bathrooms', Bathrooms);
+    //initEntity('FoodPlaces', FoodPlace)
+  }
+});
